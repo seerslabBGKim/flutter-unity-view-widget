f6074e3a0947841828d065a42769ce22f470bb75
Byounggoo Kim <bgkim@seerslab.com>
2023-10-19 19:24:15 +0900

    uniwebview debugging

--- example/android/app/src/main/kotlin/com/xraph/plugin/flutter_unity_widget_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/xraph/plugin/flutter_unity_widget_example/MainActivity.kt
@@ -3,4 +3,6 @@ package com.xraph.plugin.flutter_unity_widget_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    @JvmField
+    var mUnityPlayer: java.lang.Object? = null
 }
